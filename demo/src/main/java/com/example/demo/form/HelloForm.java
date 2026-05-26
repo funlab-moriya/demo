f599689ce1805b26dedc5f24f39a11f41e6e3c9e
funlab-moriya <yuko.moriya@funlab.jp>
2026-05-26 14:21:33 +0900

    初回コミット

---
+++ demo/src/main/java/com/example/demo/form/HelloForm.java
@@ -0,0 +1,16 @@
+package com.example.demo.form;
+
+public class HelloForm {
+    
+      private String name;
+  
+      // ゲッターとセッター
+      public String getName() {
+          return name;
+      }
+  
+      public void setName(String name) {
+          this.name = name;
+      }
+
+}
